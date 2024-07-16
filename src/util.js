96486d0915bc5d434f293b10081afd962fb2aaee
DaniParkHoJin <parkhojin@naver.com>
2024-07-16 16:33:57 +0900

    Home 페이지 구현 중

--- src/util.js
+++ src/util.js
@@ -4,6 +4,7 @@ import emotion3 from "./img/emotion3.png";
 import emotion4 from "./img/emotion4.png";
 import emotion5 from "./img/emotion5.png";
 
+
 export const getEmotionsImgById = (emotionId) => {
     const targetEmotionId = String(emotionId);
     switch (targetEmotionId) {
@@ -22,4 +23,51 @@ export const getEmotionsImgById = (emotionId) => {
             return null;
 
     }
+};
+export const emotionList = [
+    {
+        id: 1,
+        name: "완전 좋음",
+        img: getEmotionsImgById(1)
+    },
+    {
+        id: 2,
+        name: "좋음",
+        img: getEmotionsImgById(2)
+    },
+    {
+        id: 3,
+        name: "그럭저럭",
+        img: getEmotionsImgById(3)
+    },
+    {
+        id: 4,
+        name: "나쁨",
+        img: getEmotionsImgById(4)
+    },
+    {
+        id: 5,
+        name: "끔찍함",
+        img: getEmotionsImgById(5)
+    },
+
+];
+
+export const getFormattedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+    return `${year}-${month}-${date}`;
+};
+
+export const getMonthRangeByDate = (date) => {
+    const beginTimeStamp = new Date(date.getFullYear(), date.getMonth(), 1).getTime();
+    const endTimeStamp = new Date(date.getFullYear(), date.getMonth() +1, 0, 23, 59, 59).getTime();
+    return {beginTimeStamp, endTimeStamp};
 };
